1fe19f8450d0045fbbe2f1efba7a077727c7cfec
Henry Abejirin <abejirihenry@gmail.com>
2025-04-13 10:48:21 +0100

    updated script

---
+++ JSlearning/anstAssign.js
@@ -0,0 +1,20 @@
+//Declaring two variables 
+let num1 = 10;
+let num2 = 20;
+
+//OPERATORS: 
+//Addition operator
+let addition = num1 + num2;
+console.log (addition);
+//subtraction operator
+let subtraction = num1 - num2;
+console.log (subtraction);
+//Multiplication operator
+let multiply = num1 * num2;
+console.log (multiply);
+//Division operator
+let division = num1 / num2;
+console.log (division);
+//Modulus operator
+let modulus = num1 % num2;
+console.log (multiply);
--- jswhenry.js
+++ jswhenry.js
@@ -3,3 +3,4 @@ let b = 15;
 console.log (a+b);
 let c = 20; 
 let d = 30; 
+console.log (c*a);
